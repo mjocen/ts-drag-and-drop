06d6296b113d4935e998c2f8997b43b58bcccac2
jocen <joma.pagayona@gmail.com>
2020-10-19 23:27:56 +1100

    module 130: filtering projects with enums

--- src/app.ts
+++ src/app.ts
@@ -132,7 +132,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.ACTIVE;
+        }
+        return prj.status === ProjectStatus.FINISHED;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -143,7 +149,8 @@ class ProjectList {
   private renderProjects() {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
-    )! as HTMLUListElement;
+	)! as HTMLUListElement;
+	listEl.innerHTML = '';
     for (const priItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = priItem.title;
