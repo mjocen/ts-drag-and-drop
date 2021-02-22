fb621dcc006850ee697371cff6b88d7c98de3fe2
Jocen Pagayona <joma.pagayona@gmail.com>
2021-02-22 22:57:00 +1100

    REFACTOR: remove extension for webpack integration

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Component } from './base-component.js';
-import { ProjectItem } from './project-item.js';
-import { Project, ProjectStatus } from '../models/project.js';
-import { autobind } from '../decorators/autobind.js';
-import { projectState } from '../state/project.js';
-import { DragTarget } from '../models/drag-drop.js';
+import { Component } from './base-component';
+import { ProjectItem } from './project-item';
+import { Project, ProjectStatus } from '../models/project';
+import { autobind } from '../decorators/autobind';
+import { projectState } from '../state/project';
+import { DragTarget } from '../models/drag-drop';
 
 // ProjectList Class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget{
